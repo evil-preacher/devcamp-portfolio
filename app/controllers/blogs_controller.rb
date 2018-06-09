f59bf8a595bed44fe958c57396c7e010a93fcaf4
Paul Pukhtiy <priest1406@gmail.com>
2018-06-09 18:53:36 +0600

    Build out topic views

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -93,6 +93,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :blog_id)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
