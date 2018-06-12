2795a175f1fbb56ac81702337ec78ef94deb02ce
Paul Pukhtiy <priest1406@gmail.com>
2018-06-12 15:06:30 +0600

    Change app title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Paul Pukhtiy | My Portfolio Website"
     @seo_keywords = "Paul Pukhtiy portfolio"
   end
 end
